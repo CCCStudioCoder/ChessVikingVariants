a7104a104b532f34ccfec72a134c986800fb9f6b
CCC Studio <ccc.studio.coder@gmail.com>
2026-06-29 11:29:31 +0200

    Typo in Variant.java

--- api/src/main/java/com/cccstudio/chess_viking_variants/api/Variant.java
+++ api/src/main/java/com/cccstudio/chess_viking_variants/api/Variant.java
@@ -2,7 +2,7 @@ package com.cccstudio.chess_viking_variants.api;
 
 public interface Variant {
 
-    Game createGame(PlayContext ctx);
+    Board createGame(PlayContext ctx);
 
     String getName();
 
